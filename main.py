6e8e07d05fe1987a613fde710eb7df7f6a6cd6c0
ItzChaBoiGhoza <cguy925@gmail.com>
2025-04-15 14:48:40 -0600

    Initial commit of the project

---
+++ main.py
@@ -0,0 +1,1 @@
+import src/gamefunctions import *
---
+++ src/gamefunctions.py
@@ -0,0 +1,34 @@
+from IPython.display import clear_output
+import random
+
+# Display the Tic Tac Toe board
+def display_board(board):
+    pass
+
+# Player choose a marker either 'X' or 'O'
+def player_marker():
+    pass
+
+# Place the player's marker at choosen position
+def place_marker(board, marker, position):
+    pass
+
+# Check to see of the picked position is free, if free return TRUE, if taken return FALSE
+def space_check(board, position):
+    pass
+
+# Player choose the position where they want to place the marker, return the picked position
+def player_choice(board):
+    pass
+
+# Check win if there are three of the same markers in a row
+def win_check(board):
+    pass
+
+# Choose who goes first with randomization
+def choose_player():
+    pass
+
+# Asks if the player wants to play again
+def replay():
+    pass
