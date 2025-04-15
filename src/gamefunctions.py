b9a849c83f43ae3962337ca9184d769975659aa9
ItzChaBoiGhoza <cguy925@gmail.com>
2025-04-15 14:53:17 -0600

    Adding final function to run the game

--- src/gamefunctions.py
+++ src/gamefunctions.py
@@ -22,7 +22,11 @@ def player_choice(board):
     pass
 
 # Check win if there are three of the same markers in a row
-def win_check(board):
+def win_check(board,marker):
+    pass
+
+# Check if the board is full, return TRUE if board is full, return FALSE if board is not full
+def full_board_check(board):
     pass
 
 # Choose who goes first with randomization
@@ -32,3 +36,4 @@ def choose_player():
 # Asks if the player wants to play again
 def replay():
     pass
+
