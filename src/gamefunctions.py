9a7ed430107aa24dca48d6ccac48d5a1c79d879f
ItzChaBoiGhoza <cguy925@gmail.com>
2025-04-18 09:45:51 -0600

    Designed the TicTacToe board layout, as well as creating gitignore

--- src/gamefunctions.py
+++ src/gamefunctions.py
@@ -3,8 +3,12 @@ import random
 
 # Display the Tic Tac Toe board
 def display_board(board):
-    pass
-
+    print(' ' + board[7] + '  |  ' + board[8] + '  |  ' + board[9] + ' ')
+    print('--- | --- | ---')
+    print(' ' + board[4] + '  |  ' + board[5] + '  |  ' + board[6] + ' ')
+    print('--- | --- | ---')
+    print(' ' + board[1] + '  |  ' + board[2] + '  |  ' + board[3] + ' ')
+    
 # Player choose a marker either 'X' or 'O'
 def player_marker():
     pass
